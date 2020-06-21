5da570a11e1078163ed1c1197487577425554a42
coseo12 <momo_12@naver.com>
2020-06-21 21:01:24 +0900

    git init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Router from 'components/Router';
+import GlobalStyles from 'components/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <Router />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Router.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import Home from 'routes/Home';
+import Search from 'routes/Search';
+import TV from 'routes/Tv';
+import Header from 'components/Header';
+
+export default () => (
+  <Router>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
+  </Router>
+);
