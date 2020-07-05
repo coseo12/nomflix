2c06f1359f100c8a4621a29bcd6a8aff05e5053d
coseo12 <momo_12@naver.com>
2020-07-05 18:10:05 +0900

    중간 저장

--- src/routes/Search/SearchPresenter.js
+++ src/routes/Search/SearchPresenter.js
@@ -1,1 +1,73 @@
-export default () => 'Search';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Loader from 'components/Loader';
+import Section from 'components/Section';
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  all: unset;
+  font-size: 28px;
+  width: 100%;
+`;
+
+const SearchPresenter = ({
+  movieResults,
+  tvResults,
+  searchTerm,
+  handleSubmit,
+  updateTerm,
+  loading,
+  error,
+}) => (
+  <Container>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        placeholder="Search Movies or TV Shows..."
+        value={searchTerm}
+        onChange={updateTerm}
+      />
+    </Form>
+    {loading ? (
+      <Loader />
+    ) : (
+      <>
+        {movieResults && movieResults.length > 0 && (
+          <Section title="movies Results">
+            {movieResults.map(movie => (
+              <span key={movie.id}>{movie.title}</span>
+            ))}
+          </Section>
+        )}
+        {tvResults && tvResults.length > 0 && (
+          <Section title="Tv Show Results">
+            {tvResults.map(show => (
+              <span key={show.id}>{show.name}</span>
+            ))}
+          </Section>
+        )}
+      </>
+    )}
+  </Container>
+);
+
+SearchPresenter.propTypes = {
+  movieResults: PropTypes.array,
+  tvResults: PropTypes.array,
+  searchTerm: PropTypes.string,
+  handleSubmit: PropTypes.func.isRequired,
+  updateTerm: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default SearchPresenter;
--- src/routes/Tv/TvPresenter.js
+++ src/routes/Tv/TvPresenter.js
@@ -1,1 +1,47 @@
-export default () => 'Tv?';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Section from 'components/Section';
+import Loader from 'components/Loader';
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const TvPresenter = ({ airingToday, topRated, popular, loading, error }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rate Shows">
+          {topRated.map(show => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Shows">
+          {popular.map(show => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="AiringToday Shows">
+          {airingToday.map(show => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
+TvPresenter.propTypes = {
+  airingToday: PropTypes.array,
+  topRated: PropTypes.array,
+  popular: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default TvPresenter;
